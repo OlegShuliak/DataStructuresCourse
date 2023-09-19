86c909dfa513082a9ee88dbb628b3be78219bbe1
Oleg  Shuliak <nformals@gmail.com>
2023-09-19 20:05:50 +0300

    hometask 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import lesson2.HeapSort;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int arr[] = {12, 11, 13, 5, 6, 7};
+        int n = arr.length;
+
+        HeapSort ob = new HeapSort();
+        ob.sort(arr);
+
+        System.out.println("Sorted array is");
+        printArray(arr);
+    }
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
     }
 }
